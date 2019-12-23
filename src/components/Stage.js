9f57ce9b13587582ccae8c2e91f63442c080006d
Ada Płoska <adawagrocka@wp.pl>
2019-12-23 11:11:31 +0100

    cells

--- src/components/Stage.js
+++ src/components/Stage.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 import Cell from "./Cell";
 const Stage = ({ stage }) => {
+  console.log(stage);
   return (
     <div>
-      <Cell />
+      {stage.map(row => row.map((cell, x) => <Cell key={x} type={cell[0]} />))}
     </div>
   );
 };
