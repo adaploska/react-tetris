a77f07e64a7b0d1b7dcac1950cef9c4868fc4a0a
Ada Płoska <adawagrocka@wp.pl>
2019-12-31 13:03:18 +0100

    rotate function

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,6 +1,6 @@
-import React, { useState, checkCollision } from "react";
+import React, { useState } from "react";
 
-import { createStage } from "../gameHellpers";
+import { createStage, checkCollision } from "../gameHellpers";
 //styled components
 import { StyledTetrisWrapper, StyledTetris } from "./styles/StyledTetris";
 //custom hooks
@@ -13,15 +13,15 @@ import Display from "./Display";
 import StartButton from "./StartButton";
 
 const Tetris = () => {
+  const [rotateElement] = usePlayer();
   const [dropTime, setDropTime] = useState(null);
   const [gameOver, setGameOver] = useState(false);
   const [player, updatePlayerPos, resetPlayer] = usePlayer();
   const [stage, setStage] = useStage(player, resetPlayer);
   const movePlayer = dir => {
-    updatePlayerPos({ x: dir, y: 1 });
-    // if (!checkCollision(player, stage, { x: dir, y: 0 })) {
-    //   updatePlayerPos({ x: dir, y: 0 });
-    // }
+    if (!checkCollision(player, stage, { x: dir, y: 0 })) {
+      updatePlayerPos({ x: dir, y: 0 });
+    }
   };
   const startGame = () => {
     //reset everything
@@ -30,34 +30,86 @@ const Tetris = () => {
     resetPlayer();
     setGameOver(false);
   };
+
   const drop = () => {
-    updatePlayerPos({ x: 0, y: 1, collided: false });
+    if (!checkCollision(player, stage, { x: 0, y: 1 })) {
+      updatePlayerPos({ x: 0, y: 1, collided: false });
+    } else {
+      // Game Over
+      if (player.pos.y < 1) {
+        console.log("GAME OVER!!!");
+        setGameOver(true);
+        setDropTime(null);
+      }
+      updatePlayerPos({ x: 0, y: 0, collided: true });
+    }
   };
-  // const drop = () => {
-  //   if (!checkCollision(player, stage, { x: 0, y: 1 })) {
-  //     updatePlayerPos({ x: 0, y: 1, collided: false });
-  //   } else {
-  //     // Game Over
-  //     if (player.pos.y < 1) {
-  //       console.log("GAME OVER!!!");
-  //       setGameOver(true);
-  //       setDropTime(null);
-  //     }
-  //     updatePlayerPos({ x: 0, y: 0, collided: true });
-  //   }
-  // };
   const dropPlayer = () => {
     drop();
   };
+  const rotateE = player => {
+    let initial = [
+      [0, "L", 0],
+      [0, "L", 0],
+      [0, "L", "L"]
+    ];
+    console.log("rotacja");
+    console.log(player);
+    // for (let i in player.tetromino) {
+    //   console.log(player.tetromino[i]);
+    //   for (let j in player.tetromino[i]) {
+    //     if (player.tetromino[i][j] === "L" && player.tetromino === initial) {
+    //       console.log("player.tetromino = initial");
+    //     }
+    //   }
+    // }
+  };
 
   const move = ({ keyCode }) => {
+    console.log(keyCode);
     if (!gameOver) {
       if (keyCode === 37) {
+        console.log("37 lewo");
         movePlayer(-1);
       } else if (keyCode === 39) {
+        console.log("39 prawo");
         movePlayer(1);
       } else if (keyCode === 40) {
+        console.log("40 dol");
         dropPlayer();
+      } else if (keyCode === 38) {
+        rotateE(player);
+
+        // console.log(player);
+        // let initial = [
+        //   [0, "L", 0],
+        //   [0, "L", 0],
+        //   [0, "L", "L"]
+        // ];
+        // let rotateFirst = [
+        //   ["L", "L", "L"],
+        //   ["L", 0, 0],
+        //   [0, 0, 0]
+        // ];
+        // let rotateSecond = [
+        //   [0, 0, 0],
+        //   [0, 0, "L"],
+        //   ["L", "L", "L"]
+        // ];
+        // let rotateFird = [
+        //   [0, "L", "L"],
+        //   [0, 0, "L"],
+        //   [0, 0, "L"]
+        // ];
+
+        // for (let i in player.tetromino) {
+        //   console.log(player.tetromino[i]);
+        //   for (let j in player.tetromino[i]) {
+        //     if (player.tetromino[i][j] === "L") {
+        //       console.log(player.tetromino[i][j]);
+        //     }
+        //   }
+        // }
       }
     }
   };
--- src/gameHellpers.js
+++ src/gameHellpers.js
@@ -8,8 +8,10 @@ export const createStage = () =>
 
 export const checkCollision = (player, stage, { x: moveX, y: moveY }) => {
   for (let y = 0; y < player.tetromino.length; y += 1) {
+    console.log(player.tetromino[y]);
     for (let x = 0; x < player.tetromino[y].length; x += 1) {
       // 1. Check that we're on an actual Tetromino cell
+      console.log(player.tetromino[y][x]);
       if (player.tetromino[y][x] !== 0) {
         if (
           // 2. Check that our move is inside the game areas height (y)
